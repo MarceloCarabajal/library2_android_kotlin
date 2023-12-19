3c12d0db73b0fbea771092cee2397ff0d43a883f
MarceloCarabajal <marce.tsgia@gmail.com>
2023-12-19 10:35:47 -0300

    logica en MainActivity para utilizar libreria importada de otro proyecto

--- ProyectoPrueba2/app/src/main/java/com/marce/libreriaprueba2/MainActivity.java
+++ ProyectoPrueba2/app/src/main/java/com/marce/libreriaprueba2/MainActivity.java
@@ -2,14 +2,25 @@ package com.marce.libreriaprueba2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.marce.librarytest1.TemperaturaFragment;
 
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager mManager;
+    private TemperaturaFragment mFragment;
+    private FragmentTransaction mTransaction;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mManager = getSupportFragmentManager();
+        mFragment = new TemperaturaFragment();
+        mTransaction = mManager.beginTransaction();
+        mTransaction.replace(R.id.fragment, mFragment);
+        mTransaction.commit();
     }
 }
